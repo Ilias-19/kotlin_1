8ab5caa0d5f20373325f534682ac1af0501a1628
Ilias-19 <69040249+Ilias-19@users.noreply.github.com>
2025-09-18 19:16:26 +0400

    super final version

--- app/src/main/java/com/example/kotlinlesson1/Main.kt
+++ app/src/main/java/com/example/kotlinlesson1/Main.kt
@@ -94,36 +94,3 @@ fun main() {
     doAction(act, authorization)
 
 }
-
-fun buy(publication: Publication) {
-    println("The purchase is complete. The purchase amount was ${publication.price}")
-}
-
-
-fun User.adultValidation() {
-    if (this.age >= 18) {
-        println("User: $this - is over 18 years old")
-    } else throw RuntimeException("error adult validation")
-}
-
-inline fun auth(user: User, authorization: AuthCallback, updateCache: () -> Unit) {
-    try {
-        user.adultValidation()
-        authorization.authSuccess()
-        updateCache()
-    }
-    catch (e: Exception) {
-        authorization.authFailed()
-    }
-}
-
-
-fun doAction(act : Action, authorization : AuthCallback) {
-    when (act) {
-        is Registration -> println("Registration started")
-        is Login -> auth(act.user, authorization) {
-            println("cache updated")
-        }
-        is Logout -> println("Logout")
-    }
-}
